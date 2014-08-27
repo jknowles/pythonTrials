cd8b63cca5d869d1526f7895853b0d23621d3271
Jared Knowles <jknowles@gmail.com>
2014-08-26 21:50:43 -0500

    updates

--- my.py
+++ my.py
@@ -1,7 +1,13 @@
-#import pandas
-import csv
-with open('bball40.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print row
+import pandas as pd
+import numpy as np
+
+path = 'bball40.csv'
+
+dat = read_csv(path, index_col=False)
+
+
+# with open('bball40.csv', 'rb') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print row
 
